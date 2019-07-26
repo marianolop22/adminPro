0356879499f6f08ccce4e17a4b7d7faa225e19dc
marianolop22 <marianolop22@yahoo.com.ar>
2019-07-26 16:50:00 -0300

    avance

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
 
+  constructor() {
+  }
+  
   ngOnInit() {
+    init_plugins();
+
+    this.form = new FormGroup ( {
+      nombre: new FormControl(
+        null,
+        Validators.required
+      ),
+      correo: new FormControl(
+        null, 
+        [Validators.required, Validators.email]
+      ),
+      password: new FormControl(
+        null,
+        Validators.required
+      ),
+      password2: new FormControl(
+        null,
+        Validators.required
+      ),
+      condiciones: new FormControl(
+        false
+      )
+    }, {
+      validators: this.areEquals('password','password2')
+    } );
+  }
+
+  registerUser () {
+    console.log ( this.form.value );
+
+    console.log (this.form.valid );
+
+    if ( this.form.invalid ) {
+      return;
+    }
+
+    if ( !this.form.value.condiciones ) {
+      console.log ('debe aceptar las condiciones');
+      return;
+    }
+
+  }
+
+  areEquals ( field1: string, field2: string) {
+
+    return ( group: FormGroup ) => {
+
+      let pass1 = group.controls[field1].value;
+      let pass2 = group.controls[field2].value;
+
+      if ( pass1 === pass2 ) {
+        return null;
+      }
+
+      return {
+        areEquals: true
+      };
+
+    };
+
+
   }
 
 }
